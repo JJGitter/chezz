719cd56e373deda52ad24d29c1fcdc28acf83086
Joel Joelsson <joeljoels93@gmail.com>
2023-04-03 11:04:41 +0200

    Added color and time control selection

--- src/Components/Lobby.js
+++ src/Components/Lobby.js
@@ -1,50 +1,7 @@
-
-
 function Lobby() {
-
-  // const [message, setMessage] = useState("");
-  // const [receivedMessage, setReceivedMessage] = useState(null);
-
-  // const sendMessage = async () => {
-  //   if (message !== "") {
-  //     const messageData = {
-  //       room: room,
-  //       sendingUser: user,
-  //       message: message,
-  //       time:
-  //         new Date(Date.now()).getHours() +
-  //         ":" +
-  //         new Date(Date.now()).getMinutes(),
-  //     };
-  //     await socket.emit("send_message", messageData);
-  //   }
-  // };
-
-  // const DisplayMessage = ({ data }) => {
-  //   console.log(data.sendingUser + " says: " + data.message);
-  //   const messageContent = data.sendingUser + " says: " + data.message;
-
-  //   return (
-  //     <>
-  //       <div>{messageContent}</div>
-  //       <div>{data.time}</div>
-  //     </>
-  //   );
-  // };
-
-
-
   return (
     <div>
       <h1>Waiting for opponent to join...</h1>
-      {/* <h2>Chat Message</h2>
-      <input
-        type={"text"}
-        value={message}
-        onChange={(input) => setMessage(input.target.value)}
-      ></input>
-      <button onClick={sendMessage}>Send</button>
-      {receivedMessage ? <DisplayMessage data={receivedMessage} /> : null} */}
     </div>
   );
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,12 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { userContext } from "../App";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [selectedColor, setSelectedColor] = useState("random");
+  const [selectedTimeControl, setSelectedTimeControl] = useState("rapid");
+  const [userColor, setUserColor] = useState(Math.random() >= 0.5 ? "white" : "black");
 
   const { user, setUser, room, setRoom, joinRoom, socket } =
     useContext(userContext);
@@ -18,55 +21,140 @@ const Login = () => {
   return (
     <div>
       <h1>CHEZZ</h1>
-      <h2>Local Game</h2>
-      <div>
-        <button
-          onClick={() => {
-            navigate("/chezz");
-          }}
-        >
-          Play Locally
-        </button>
-        <h2>Online Game</h2>
-        <div>
-          Enter your name:
-          <input
-            type="text"
-            value={user}
-            onChange={(input) => {
-              setUser(input.target.value);
-            }}
-          />
-        </div>
-        <form>
-          <div>
-            Enter game room:
-            <input
-              type="text"
-              value={room}
-              onChange={(input) => {
-                setRoom(input.target.value);
-              }}
-            />
-          </div>
-          <button
-            onClick={() => {
-              joinRoom();
-              navigate("/lobby");
-            }}
-          >
-            Start/Join Game
-          </button>
-        </form>
 
-        {user !== "" && room !== "" ? (
-          <div>
-            Join as {user} in game room {room}.
-          </div>
-        ) : null}
-      </div>
+      <LocalGameForm navigate={navigate} />
+      <OnlineGameForm
+        user={user}
+        setUser={setUser}
+        room={room}
+        setRoom={setRoom}
+        joinRoom={joinRoom}
+        navigate={navigate}
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+        selectedTimeControl={selectedTimeControl}
+        setSelectedTimeControl={setSelectedTimeControl}
+        setUserColor={setUserColor}
+      />
     </div>
   );
 };
 
 export default Login;
+
+function LocalGameForm({ navigate }) {
+  return (
+    <>
+      <h2>Local Game</h2>
+      <button
+        onClick={() => {
+          navigate("/chezz");
+        }}
+      >
+        Play Locally
+      </button>
+    </>
+  );
+}
+
+function OnlineGameForm({
+  user,
+  setUser,
+  room,
+  setRoom,
+  joinRoom,
+  navigate,
+  selectedColor,
+  setSelectedColor,
+  selectedTimeControl,
+  setSelectedTimeControl,
+  setUserColor
+}) {
+  return (
+    <>
+      <h2>Online Game</h2>
+      <div className="colorSelection">
+        <button
+          style={{ opacity: selectedColor !== "white" ? 0.5 : 1 }}
+          onClick={() => setSelectedColor("white")}
+        >
+          Play as White
+        </button>
+        <button
+          style={{ opacity: selectedColor !== "black" ? 0.5 : 1 }}
+          onClick={() => setSelectedColor("black")}
+        >
+          Play as Black
+        </button>
+        <button
+          style={{ opacity: selectedColor !== "random" ? 0.5 : 1 }}
+          onClick={() => {
+            setSelectedColor("random");
+            const randomColor = Math.random() >= 0.5 ? "white" : "black";
+            setUserColor(randomColor);
+          }}
+        >
+          Randomize Color
+        </button>
+      </div>
+      <div className="timeControlSelection">
+        <button
+          style={{ opacity: selectedTimeControl !== "classical" ? 0.5 : 1 }}
+          onClick={() => setSelectedTimeControl("classical")}
+        >
+          Classical
+        </button>
+        <button
+          style={{ opacity: selectedTimeControl !== "rapid" ? 0.5 : 1 }}
+          onClick={() => setSelectedTimeControl("rapid")}
+        >
+          Rapid
+        </button>
+        <button
+          style={{ opacity: selectedTimeControl !== "blitz" ? 0.5 : 1 }}
+          onClick={() => setSelectedTimeControl("blitz")}
+        >
+          Blitz
+        </button>
+        <button
+          style={{ opacity: selectedTimeControl !== "bullet" ? 0.5 : 1 }}
+          onClick={() => setSelectedTimeControl("bullet")}
+        >
+          Bullet
+        </button>
+      </div>
+      <form>
+        <input
+          type="text"
+          placeholder="Name..."
+          value={user}
+          onChange={(input) => {
+            setUser(input.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Room ID..."
+          value={room}
+          onChange={(input) => {
+            setRoom(input.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            joinRoom();
+            navigate("/lobby");
+          }}
+        >
+          {user !== "" && room !== "" ? (
+            <div>
+              Enter game room {room} as {user}
+            </div>
+          ) : (
+            "Create/Join Game"
+          )}
+        </button>
+      </form>
+    </>
+  );
+}
